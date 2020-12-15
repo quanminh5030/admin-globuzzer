1cded8d52e8ee77e1a2182d511510e8585946727
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-12-15 09:22:26 +0200

    break down joincity section into component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,67 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../css/Home.css";
 import { FiSearch } from "react-icons/fi";
 import LazyLoad from "react-lazyload";
 import { HomeValue } from "../components/HomeValue/HomeValue";
 import { SectionHeader } from "../components/SectionHeader/SectionHeader";
-import { JoinCity } from "../components/JoinCity/JoinCity";
 import community from "../assets/Value_community.svg";
 import expert from "../assets/Value_expert.svg";
 import journey from "../assets/Value_journey.svg";
-import AuxServicesData from "../Data/AuxServicesData";
 import AuxServiceSection from "../components/AuxService/AuxServiceSection";
 import { OwnSection } from "../components/OwnSection/OwnSection";
 import { Footer } from "../components/Footer/Footer";
 import { SearchCity } from "../components/SearchCity/SearchCity";
 import { JoinCommunity } from "../components/JoinCommunity/JoinCommunity";
-import { RequestNewCity } from "../components/RequestNewCity/RequestNewCity";
 import { Link } from "react-router-dom";
-import { firestore } from "../utils/firebase.utils";
-import CityForm from './Admin/CityForm/CityForm';
+import JoinCitySection from '../components/JoinCity/JoinCitySection';
 import FeaturedArticlePage from "../components/FeaturedArticle/FeaturedArticlePage";
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [moreJoinCity, setMoreJoinCity] = useState(false);
-  const [joinCity, setJoinCity] = useState([]);
-  const [isVisible, setIsVisible] = useState(false);
-  const initialItemState = [
-    { id: null, name: "", img: "", members: ""},
-  ];
-
-  const [currentItem, setCurrentItem] = useState(initialItemState);
-  useEffect(() => {
-    const unsubscribe=firestore
-      .collection("cities")
-      .orderBy("name")
-      .limit(11)
-      .onSnapshot((snapshot) => {
-        const newCity = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setJoinCity(newCity);
-        console.log(newCity);
-      });
-      return () => unsubscribe();
-  }, []);
-
- 
-const formHandler = (cityData) => {
-  setIsVisible(true);
-  setCurrentItem({
-   id: cityData.id,
-   name:cityData.name,
-   members: cityData.members,
-   img:cityData.img
- })
-};
-
-const updateItem = (({currentItem}, updatedItem)=> {
-  console.log("it sends item to the updated item function", updatedItem, currentItem.id);
-  setIsVisible(false);
-  firestore.collection('cities').doc(currentItem.id).update(updatedItem);
-})
-
   const one = `Globuzzer is a global network that provides the full relocating experience. 
 Find topics, join communities, attend events, book flights, and much more. `;
   const two = `Reliable information shared by expats and locals. 
@@ -123,26 +78,7 @@ Most importantly, we have been in the same spot, and we can support you. `;
             onChange={(event) => setQuery(event.target.value)}
           />
         </div>
-          <div className="joincity_grid">
-          {joinCity.map((cityData) => (
-            <JoinCity cityData={cityData} key={cityData.id} openForm={()=>formHandler(cityData)}/>
-          ))}
-          {!moreJoinCity && joinCity.length > 0 && (
-            <JoinCity
-              cityData={{ name: "Explore more cities" }}
-              isViewMore
-              setMoreJoinCity={setMoreJoinCity}
-            />
-          )}
-          {moreJoinCity && joinCity.length > 0 && <RequestNewCity />}
-          {isVisible &&  <div>
-          <CityForm setIsVisible={setIsVisible} currentItem={currentItem} updateItem={updateItem}/>                
-        </div>}  
-        </div> 
-       
-        <div className="no_item">
-          {joinCity.length === 0 && <RequestNewCity />}
-        </div>
+        <JoinCitySection/>
       </section>
       <JoinCommunity />
 
