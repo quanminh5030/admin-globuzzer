67a889f29d971e992255f6c32be24b79e8079455
Gheorghe Tarcea <tarcea@gmail.com>
2021-01-28 09:26:18 +0100

    slick carousel implemented in aux-services section

--- src/components/AuxService/AuxServiceSection.js
+++ src/components/AuxService/AuxServiceSection.js
@@ -3,6 +3,10 @@ import AuxService from './AuxService';
 import { firestore } from "../../utils/firebase.utils";
 import "../../css/Home.css";
 import ServiceForm from '../../pages/Admin/ServiceForm/ServiceForm';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 const AuxServiceSection = () => {
     const [services, setServices] = useState([]);
     const [display, setDisplay] = useState(false);
@@ -57,16 +61,57 @@ const AuxServiceSection = () => {
                 />
             </div>
         );
-    }
+    };
     
+    const settings = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
     return (
      <div className="aux_list">
+         <Slider {...settings}>
           {services.map((data) => (
             <div key={data.id}>
-                <AuxService data={data} editService={()=> displayEditForm(data)} />
+                    <AuxService 
+                        data={data} 
+                        editService={()=> displayEditForm(data)} 
+                    />
                 {onSelectedService(data, currentService)}
             </div>
           ))}
+          </Slider>
         </div>
     );
 }
